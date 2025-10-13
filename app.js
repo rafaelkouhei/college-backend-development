042a0ad0e80eb7cfd098e2a7a5e6ebe6a6f886d2
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2025-10-13 20:04:21 -0300

    removed unnecessary items and added routes and controllers for products and users

--- app.js
+++ app.js
@@ -1,65 +1,25 @@
-require("dotenv").config();
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-
-// CRUD
-// C - Create (POST)
-// R - Read (GET)
-// U - Update (PUT / PATCH)
-// D - Delete (DELETE)
-
-let products = [
-    {id: 1, name: "Mechanical Keyboard", price: 450.00},
-    {id: 2, name: "Gaming Mice", price: 150.00},
-    {id: 3, name: "Ultra Wide Screen", price: 1200.00},
-]
-
-let nextId = 4;
-
+const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORTA = 3000;
 
-// POST
-app.post("/api/products", (req, res) => {
-    const {name, price} = req.body;
+// Middleware para interpretar JSON
+app.use(express.json());
 
-    if(!name || price === undefined){
-        return res.status(400).json({message: "Name and Price are required!"});
-    }
+// Importamos o nosso arquivo de rotas
+const produtoRoutes = require('./routes/produtoRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 
-    const newProduct = {
-        id: nextId++,
-        name,
-        price
-    };
+// Usamos o roteador na nossa aplicação, definindo um prefixo
+// Todas as rotas em 'produtoRoutes' terão '/api/produtos' antes
+app.use('/api/produtos', produtoRoutes);
+app.use('/api/usuarios', usuarioRoutes);
 
-    products.push(newProduct);
-
-    res.status(201).json(newProduct);
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('API de Produtos e Usuários funcionando!');
 });
 
-// +++----------------------------------------------------------------------------------------------------------------------------+++ //
-
-// const users = [
-//     {id: 1, email: "admin@app.com", passwordhash: "", role: "admin"},
-//     {id: 2, email: "user@app.com", passwordhash: "", role: "user"}
-// ]
-
-// const verifyToken = (req, res, next) => {
-//     const authenticator = req.headers["authorization"];
-//     const token = authenticator && authenticator.split(" ")[1];
-// }
-
-// if(!token)
-// {
-//     return res.status(401).json({
-//         message: "Acesso negado. Token não fornecido."
-//     })
-// }
-
-// try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-// } catch(err) {
-//     return res.status(403)
-// }
+// Inicialização do servidor
+app.listen(PORTA, () => {
+  console.log(`Servidor rodando em http://localhost:${PORTA}`);
+});
